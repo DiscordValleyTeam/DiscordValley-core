9138dbd8061cf8b84d108da445fee9c4018cd624
Sergiu Bucur <sebu400@gmail.com>
2020-08-17 17:22:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Discord_Valley
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+        private CommandHandler Handler;
+        public async Task MainAsync()
+        {
+            Client = new DiscordSocketClient();
+            Client.Log += Log;
+            Client.MessageReceived += MessageReceived;
+            Client.Ready += Client_Ready;
+
+            Commands = new CommandService();
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+            Handler = new CommandHandler(Client, Commands);
+            await Handler.InstallCommandsAsync(Client);
+
+            Console.Title = "Discord Valley";
+
+            await Client.LoginAsync(TokenType.Bot, File.ReadAllText("token.txt"));
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready()
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        private async Task MessageReceived(SocketMessage Message)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+
+        }
+
+        public class CommandHandler
+        {
+            private readonly DiscordSocketClient _client;
+            private readonly CommandService _commands;
+
+            public CommandHandler(DiscordSocketClient client, CommandService commands)
+            {
+                _commands = commands;
+                _client = client;
+            }
+
+            public async Task InstallCommandsAsync(DiscordSocketClient client)
+            {
+                // Hook the MessageReceived event into our command handler
+                _client.MessageReceived += HandleCommandsAsync;
+                await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+            }
+
+            private async Task HandleCommandsAsync(SocketMessage messageParam)
+            {
+                // Don't process the command if it was a system message
+                var message = messageParam as SocketUserMessage;
+                if (message == null) return;
+
+                // Create a number to track where the prefix ends and the command begins
+                int argPos = 0;
+
+                // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+                if (!message.HasCharPrefix('!', ref argPos) || message.Author.IsBot)
+                {
+                    return;
+                }
+
+                // Create a WebSocket-based command context based on the message
+                var context = new SocketCommandContext(_client, message);
+                var result = await _commands.ExecuteAsync(context: context, argPos: argPos, services: null);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine($"[{DateTime.Now} at Commands] Something went wrong with a command. Text: {context.Message.Content} | Error: {result.ErrorReason}");
+                    await context.Channel.SendMessageAsync($":x: {result.ErrorReason}");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Discord Valley
+Discord Valley is a game bot inspired from Stardew Valley. It has farming mechanics controlled via commands. You can grow crops, harvest them, house animals in a barn, sell produce on the market and more!
+
+# Development
+Current state: **Alpha**
+
+[Trello page](https://trello.com/b/2GRqGOq9/discord-valley)
+
+# License
+Discord Valley is a semi-open source bot. The core files in this repository are licensed under the MIT license, however the bot has private code parts which are proprietary.
